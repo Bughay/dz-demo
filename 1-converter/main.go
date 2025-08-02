faeaff5de5dbe89a3c1fdc751fcbcbc42d75cce9
Bughay <markschrammel6520906@gmail.com>
2025-08-02 12:58:31 +0300

    second homework

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USD_TO_EUR float64 = 0.88
 	const USD_TO_RUB float64 = 81.15
 	const EUR_TO_RUB float64 = USD_TO_RUB / USD_TO_EUR
+	amount := UserInput()
+	fmt.Printf("the amount is: %v \n", amount)
 }
+
+func UserInput() (amount float64) {
+	fmt.Println("please enter the amount: ")
+	fmt.Scan(&amount)
+	return amount
+}
+
+func Calculate(amount float64, original_currency string, target_currency string) {}
