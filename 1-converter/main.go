0f9aacfa426b92271618d7016016eaee4b2488ba
Bughay <markschrammel6520906@gmail.com>
2025-08-02 18:10:08 +0300

    3-start homework 2

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -6,37 +6,40 @@ import (
 )
 
 func main() {
-	const USD_TO_EUR float64 = 0.88
-	const USD_TO_RUB float64 = 81.15
-	const EUR_TO_RUB float64 = USD_TO_RUB / USD_TO_EUR
-	amount, err := UserInput()
-	if err != nil {
-		fmt.Print(err)
+	for {
+		const USD_TO_EUR float64 = 0.88
+		const USD_TO_RUB float64 = 81.15
+		const EUR_TO_RUB float64 = USD_TO_RUB / USD_TO_EUR
+		amount, err := UserInput()
+		if err != nil {
+			fmt.Print(err)
+			continue
+		}
+		fmt.Printf("the amount is: %v \n", amount)
+		original_currency, target_currency, err := UserInputCurrency()
+		if err != nil {
+			fmt.Print(err)
+			continue
+		} else {
+			fmt.Printf("conversion amount of %v from %s to %s is: ", amount, original_currency, target_currency)
+		}
 
-	}
-	fmt.Printf("the amount is: %v \n", amount)
-	original_currency, target_currency, err := UserInputCurrency()
-	if err != nil {
-		fmt.Print(err)
-	} else {
-		fmt.Printf("conversion amount of %v from %s to %s is: ", amount, original_currency, target_currency)
-	}
-
-	switch {
-	case original_currency == "USD" && target_currency == "EUR":
-		fmt.Println(amount * USD_TO_EUR)
-	case original_currency == "USD" && target_currency == "RUB":
-		fmt.Println(amount * USD_TO_RUB)
-	case original_currency == "EUR" && target_currency == "USD":
-		fmt.Println(amount / USD_TO_EUR)
-	case original_currency == "EUR" && target_currency == "RUB":
-		fmt.Println(amount * EUR_TO_RUB)
-	case original_currency == "RUB" && target_currency == "USD":
-		fmt.Println(amount / USD_TO_RUB)
-	case original_currency == "RUB" && target_currency == "EUR":
-		fmt.Println(amount / EUR_TO_RUB)
-	default:
-		fmt.Printf("unsupported currency pair: %s to %s", original_currency, target_currency)
+		switch {
+		case original_currency == "USD" && target_currency == "EUR":
+			fmt.Println(amount * USD_TO_EUR)
+		case original_currency == "USD" && target_currency == "RUB":
+			fmt.Println(amount * USD_TO_RUB)
+		case original_currency == "EUR" && target_currency == "USD":
+			fmt.Println(amount / USD_TO_EUR)
+		case original_currency == "EUR" && target_currency == "RUB":
+			fmt.Println(amount * EUR_TO_RUB)
+		case original_currency == "RUB" && target_currency == "USD":
+			fmt.Println(amount / USD_TO_RUB)
+		case original_currency == "RUB" && target_currency == "EUR":
+			fmt.Println(amount / EUR_TO_RUB)
+		default:
+			fmt.Printf("unsupported currency pair: %s to %s", original_currency, target_currency)
+		}
 	}
 
 }
