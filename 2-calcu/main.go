e6baab2a95fcaf2309831ef8833a9b49732a3ee5
Bughay <markschrammel6520906@gmail.com>
2025-08-16 06:58:26 +0300

    lets go

--- 2-calcu/main.go
+++ 2-calcu/main.go
@@ -16,6 +16,13 @@ type BinList struct {
 	bins map[string]Bin
 }
 
+func createBinList() *BinList {
+	binlist := &BinList{
+		bins: make(map[string]Bin),
+	}
+	return binlist
+}
+
 func createBin(id string, private bool, createdAt time.Time, name string) *Bin {
 	newBin := &Bin{
 		id:        id,
@@ -27,7 +34,12 @@ func createBin(id string, private bool, createdAt time.Time, name string) *Bin {
 }
 func main() {
 
-	bin1 := createBin("5", true, time.Now(), "mark")
+	bin1 := createBin("hi", true, time.Now(), "mark")
 	fmt.Println(bin1)
+	binList := createBinList()
+	binList.bins[bin1.id] = *bin1
+	binList.bins["second"] = *bin1
+
+	fmt.Println(binList)
 
 }
